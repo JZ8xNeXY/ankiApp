6a3f164e94b8d63fbd154ce0ac5fd15295fd8b7f
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-03-12 12:57:40 +0900

    index.tsxの作成

---
+++ src/app/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Index = ():JSX.Element => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default Index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+});
