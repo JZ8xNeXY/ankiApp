e4cb7c5b2a3c47ea4398e3d1ea40d841e25d85ea
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-08-15 13:45:30 +0900

    studyHistory.tsxの作成

---
+++ src/app/history/studyHistory.tsx
@@ -0,0 +1,13 @@
+import { useRouter } from 'expo-router'
+import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  Linking,
+  ScrollView,
+} from 'react-native'
+import { auth } from '../../config'
+import Footer from '../components/footer'
