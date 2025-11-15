6b3531584f1decf06bbdb130763d44cd7ef54e66
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-11-15 15:45:27 +0900

    ステータスバーの色の修正

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from 'expo-router'
 import React from 'react'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { StatusBar } from 'expo-status-bar'
 
 const Layout = (): JSX.Element => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
+      <StatusBar style="dark" /> 
       <Stack
         screenOptions={{
           headerShown: false,
