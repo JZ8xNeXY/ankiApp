3426a24db944d052b58776e7de1eb807bba90d0a
sirokuma-2 <56890530+sirokuma-2@users.noreply.github.com>
2025-12-31 11:10:28 +0900

    ログインの挙動を修正

--- src/app/dev/mockTime.ts
+++ src/app/dev/mockTime.ts
@@ -1,5 +1,5 @@
 // モックを有効にする時間帯だけ管理する　開発時間予定時間を先に入れる
-const MOCK_START_HOUR = 13 // 13時
+const MOCK_START_HOUR = 10 // 13時
 const MOCK_END_HOUR = 17 // 17時
 
 export default function isMockTime(): boolean {
--- src/config.ts
+++ src/config.ts
@@ -1,32 +1,35 @@
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
-import { initializeApp,getApp, getApps } from 'firebase/app'
-import { initializeAuth, getAuth, getReactNativePersistence } from 'firebase/auth'
+import { initializeApp, getApp, getApps } from 'firebase/app'
+import {
+  initializeAuth,
+  getAuth,
+  getReactNativePersistence,
+} from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
-import  isMockTime  from './app/dev/mockTime'
+import isMockTime from './app/dev/mockTime'
 
 let firebaseConfig
 
 if (!isMockTime()) {
   //本番用のfirebaseConfig//
- firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FB_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FB_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_FB_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FB_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FB_APP_ID,
-}
+  firebaseConfig = {
+    apiKey: process.env.EXPO_PUBLIC_FB_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FB_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FB_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FB_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FB_APP_ID,
+  }
 } else {
-//開発用のfirebaseConfig//
- firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_DEV_FB_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_DEV_FB_AUTH_DOMAIN,
-  projectId: process.env.EXPO_PUBLIC_DEV_FB_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_DEV_FB_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_DEV_FB_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_DEV_FB_APP_ID,
-}
-
+  //開発用のfirebaseConfig//
+  firebaseConfig = {
+    apiKey: process.env.EXPO_PUBLIC_DEV_FB_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_DEV_FB_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_DEV_FB_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_DEV_FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_DEV_FB_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_DEV_FB_APP_ID,
+  }
 }
 
 // Appは二重初期化しない
@@ -35,17 +38,15 @@ const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
 let auth
 
 try {
-  auth = getAuth(app)
-} catch {
   auth = initializeAuth(app, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage),
   })
+} catch (error: any) {
+  auth = getAuth(app)
 }
 
 const db = getFirestore(app)
 
-console.log(
-  !isMockTime() ? '🔥 Firebase: PROD/MOCK' : '🧪 Firebase: DEV'
-)
+console.log(!isMockTime() ? '🔥 Firebase: PROD/MOCK' : '🧪 Firebase: DEV')
 
 export { app, auth, db }
